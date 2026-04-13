052408da7660e658a1dae6a92b490cb6e53050b2
Joanne Wardell <joanne.wardell6@gmail.com>
2026-04-13 16:54:58 +0000

    add seed weights and slurm scripts for fc-option2 dense+masked fbirn

---
+++ slurm_scripts/fbirn/fbirn_fc_option2_dense.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 24
+#SBATCH --mem=100g
+#SBATCH -p qTRDGPUH
+#SBATCH -t 7200
+#SBATCH --gres=gpu:A100:1
+#SBATCH -J fc_opt2_dense
+#SBATCH -D /data/users2/jwardell1/multimodal-subnetworks-fc-option2
+#SBATCH --output=/data/users2/jwardell1/multimodal-subnetworks-fc-option2/_out/%j.out
+#SBATCH -A psy53c17
+
+sleep 10s
+echo "Running on host: $HOSTNAME"
+echo "Job ID: $SLURM_JOB_ID"
+export TMPDIR=/tmp
+source /data/users2/jwardell1/miniconda3/bin/activate mmsn312
+
+python3 train_script_rev.py \
+    --config-name new_conf \
+    --config-dir conf \
+    experiment.experiment_name=fbirn_multimodal_dense_fc_option2 \
+    experiment.collections=fbirn \
+    experiment.dbfields=[falff,smri,dwi] \
+    experiment.metafields=[gender_encoded] \
+    experiment.epochs=60 \
+    experiment.cv_folds=10 \
+    experiment.num_workers=16 \
+    model.masked=False \
+    model.model_channels=64 \
+    model.init_weights_path=/data/users2/jwardell1/multimodal-subnetworks-fc-option2/init_weights_seed1997_fc_option2.pth
+
+echo "Job $SLURM_JOB_ID completed"
